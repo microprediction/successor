91a91945c28aca16902d4b255b0006e6c674ed79
Peter Cotton <peter.cotton@microprediction.com>
2021-10-13 15:49:02 -0400

    ver

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="successor",
-    version="0.1.4",
+    version="0.2.0",
     description="Predict the next number in a sequence, or the next k",
     long_description=README,
     long_description_content_type="text/markdown",
